f63a797d771bc30e7db07374abf24d28dbd04bbe
amaury0596 <amauryLechaffotec@outlook.fr>
2019-02-24 12:52:36 +0100

    ballon avec score :q
    :q
    :qa

--- ballon.py
+++ ballon.py
@@ -1,4 +1,5 @@
 import pygame , os
+from threading import Thread, RLock
 
 pygame.init()
 pygame.font.init( ) 
@@ -19,20 +20,199 @@ nuage = 300
 
 
 
+
+
+
+
+
+
+
+
 surface = pygame.display.set_mode((surfaceW,surfaceH))
 
 pygame.display.set_caption('ballon volant')
 
-img = pygame.image.load('Ballon01.png')
-imgNuageH = pygame.image.load('NuageHaut.png')
+img = pygame.image.load('Ballon01.png').convert_alpha( )
+
+
+imgNuageH = pygame.image.load('NuageHaut.png').convert_alpha( )
 imgNuageB = pygame.image.load('NuageBas.png')
 
-
-print(img)
-
-
+surfaceBallon = pygame.Surface((50,60))
+surfaceNuage = pygame.Surface((300,300))
 
 
+
+print(surface)
+
+
+
+
+	
+
+# thread nuage 
+
+# class nuageHT(Thread):
+
+# 	def __init__(self):
+
+# 		Thread.__init__(self)
+		
+
+# 	def run(self):
+
+		
+
+			
+
+		
+
+# 		global pos_nuage
+
+# 		pos_nuage += -0.5
+
+# 		nuageH(pos_nuage,0,imgNuageH)
+
+		
+
+		
+
+
+			
+
+
+
+		
+
+
+
+
+
+
+
+
+# class ballonT(Thread):
+
+# 	def __init__(self):
+
+# 		Thread.__init__(self)
+
+# 	def run(self):
+
+# 		global x
+# 		global y
+# 		global y_mouvement
+# 		global appuy
+
+
+		
+
+		
+
+
+
+
+
+# 		ballon(x,y,img)
+		
+	
+
+
+		
+
+
+
+# 		for event in pygame.event.get():
+
+			
+
+
+
+# 			if event.type == pygame.KEYDOWN:
+# 				if event.key == pygame.K_UP :
+
+# 					appuy = True
+
+# 					y_mouvement = -0.5
+
+					
+
+
+# 			if event.type == pygame.KEYUP and appuy:
+
+
+
+# 				y_mouvement = 0.5
+
+
+
+# 		y += y_mouvement
+
+# 		if y > surfaceH -40 or y < -surfaceH -10:
+
+# 			gameover()
+
+
+		
+
+				
+
+		
+
+
+					    
+
+
+
+
+
+
+
+		
+
+
+
+		
+
+
+
+		
+
+
+
+
+			
+
+					
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# fin thread nuage
+
+
+# partie perdu 
 
 def gameover():
 
@@ -47,9 +227,11 @@ def gameover():
 	while True :
 
 
-		pygame.font.init( ) 
+		pygame.font.init( )
 
-	
+
+
+
 
 
 
@@ -89,49 +271,77 @@ def gameover():
 			if event.type == pygame.KEYDOWN :
 
 
-				print('continué')
+					print('continué')
+					principal()
 
 
 
-				principal()
-
-
-
-                           
-
-                            
-			    
-
-                                
-
-                        
-
-                
+					
 
 	
 
-	
+
+
+
+
+
+
+					
+
+
+
+
+
+
+
+
+
+
+
+
+
+# fin partie perdu 
+
+
+
 
 def nuageH(x,y,image):
 
-	surface.blit(image,(x,y))
+
+	return surface.blit(image,(x,y))
+
 
 def nuageB(x,y,image):
 
+
 	surface.blit(image,(x,y))
 
+		
 
-	
 
 
-	
+
+
+
 
 def ballon(x,y,image):
+
 
 	surface.blit(image,(x,y))
 
 
 def principal():
+
+	
+	global x
+	global y
+	global y_mouvement
+	global appuy
+
+	
+
+
+	
 
 
 
@@ -144,54 +354,147 @@ def principal():
 
 	appuy = False
 
+	global pos_nuage
+
 	pos_nuage = 800
+
+	nuageoff = False
+
+	global score 
+
+	score = 0 
+
+
+
+	# thread_2 = ballonT()
 
 	
 
-	while not game_over :
 
-		pos_nuage += -0.5
+
+
+
+	
+
+	
+
+
+
+	while not game_over:
+
+
 
 		
 
+		if pos_nuage < -350 :
+
+			pos_nuage = 800
 		
+		
+
+
+
+
+	
 
 		surface.fill(blue)
 
+		# thread_1 = nuageHT()
+		# thread_2 = ballonT()
+
+
+		# thread_1.start()
+		# thread_2.start()
+
+		# thread_1.join()
+		# thread_2.join()
+
+
+
+		
+
+	
+
+		
+
+
+	    
+
+
+
+
+
+
+
+
+		
+
+		
+
+		
+
+		
+
 		ballon(x,y,img)
-		nuageH(pos_nuage,0,imgNuageH)
+		nuageH(pos_nuage,-50,imgNuageH)
 
 
+		
 
-		pygame.display.update()
+		if nuageoff:
+
+			pos_nuage += -0.1
+
+		
 
 
 
 		for event in pygame.event.get():
-				
-				
-				
+
+
+
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_UP :
 
 					appuy = True
 
-					y_mouvement = -0.5
+					nuageoff = True
+
+					y_mouvement = -0.4
 
 
 			if event.type == pygame.KEYUP and appuy:
 
-				
 
-				y_mouvement = 0.5
+
+				y_mouvement = 0.4
 
 
 
 		y += y_mouvement
 
 		if y > surfaceH -40 or y < -surfaceH -10:
-			gameover()
 			
+			print('Votre score est de : ' ,score)
+			gameover()
+
+
+		if y  < 200 and (200 > pos_nuage and pos_nuage > -100) :
+
+			
+			print('Votre score est de : ' ,score)
+			gameover()
+
+		if (pos_nuage > 50 and pos_nuage < 50.1 )  :
+
+			score += 1 
+
+		
+
+
+
+
+		pygame.display.flip()
 
 
 
@@ -208,6 +511,6 @@ def principal():
 
 
 principal()
-       
+
 
 
